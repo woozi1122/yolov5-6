bcb73c357e9a14873bf851d7931288f302c29914
woozi1122 <87755876+woozi1122@users.noreply.github.com>
2021-11-04 17:32:01 +0800

    Update common.py

--- models/common.py
+++ models/common.py
@@ -567,3 +567,18 @@ class ResBlock_CBAM(nn.Module):
         out = self.relu(out)
         return out
         
+class h_sigmoid(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_sigmoid, self).__init__()
+        self.relu = nn.ReLU6(inplace=inplace)
+
+    def forward(self, x):
+        return self.relu(x + 3) / 6
+
+class h_swish(nn.Module):
+    def __init__(self, inplace=True):
+        super(h_swish, self).__init__()
+        self.sigmoid = h_sigmoid(inplace=inplace)
+
+    def forward(self, x):
+        return x * self.sigmoid(x)
