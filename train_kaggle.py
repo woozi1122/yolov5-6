f5c9fa0afe796674e4520ee1ef3dbb971e4704af
woozi1122 <87755876+woozi1122@users.noreply.github.com>
2021-11-11 17:37:27 +0800

    Update train_kaggle.py

--- train_kaggle.py
+++ train_kaggle.py
@@ -70,7 +70,7 @@ def train(hyp,  # path/to/hyp.yaml or hyp dictionary
     save_dir = Path('./')
     w = save_dir / 'weights'  # weights dir
     (w.parent if evolve else w).mkdir(parents=True, exist_ok=True)  # make dir
-    last,best = w / 'last.pt', w / 'best.pt'
+    last,best = save_dir / 'last.pt', save_dir / 'best.pt'
 
     # Hyperparameters
     if isinstance(hyp, str):
